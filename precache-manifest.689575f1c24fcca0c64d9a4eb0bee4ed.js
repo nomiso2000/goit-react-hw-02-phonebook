4be9c4caf91bd6d2bb07d0a2b707cc763a5e9def
nomiso2000 <nomiso.study@gmail.com>
2020-09-22 17:08:02 +0300

    Updates

---
+++ precache-manifest.689575f1c24fcca0c64d9a4eb0bee4ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61b3c86f7017c57c360ad871d80ac6da",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "5a5e6079139c7bdd420e",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.40aa7c02.chunk.css"
+  },
+  {
+    "revision": "4b358acba9e600d36977",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.1e1831e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.1e1831e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a5e6079139c7bdd420e",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.7b6ee316.chunk.js"
+  },
+  {
+    "revision": "1d31fee250c9b83d1791",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.0dacc542.js"
+  }
+]);
